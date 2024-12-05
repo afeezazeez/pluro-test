0fe2c89e0e4f4fa5e9f1b54c03b12d9cfad4e9dd
chillingloccini <chillingloccini1@gmail.com>
2024-12-05 01:00:32 +0100

    Header Nesting issues check implementation

--- app/Services/AccessibilityAnalyzerService.php
+++ app/Services/AccessibilityAnalyzerService.php
@@ -25,6 +25,7 @@ class AccessibilityAnalyzerService
 
         $issues = [
             'text_alternatives' => $this->checkMissingAlt($xpath),
+            'adaptable' => $this->checkSkippedHeadings($xpath),
         ];
 
         $complianceScore = $this->calculateComplianceScore($issues);
@@ -51,6 +52,34 @@ class AccessibilityAnalyzerService
                 'element' => $img->ownerDocument->saveHTML($img),
                 'suggested_fix' => 'Add an alt attribute to your img element.',
             ];
+        }
+
+        return $issues;
+    }
+
+    /**
+     * Check for skipped heading levels.
+     * @param DOMXPath $xpath
+     * @return array
+     */
+    private function checkSkippedHeadings(DOMXPath $xpath): array
+    {
+        $issues = [];
+        $headings = $xpath->query('//h1|//h2|//h3|//h4|//h5|//h6');
+
+        $previousLevel = 0;
+
+        foreach ($headings as $heading) {
+            $currentLevel = (int)substr($heading->nodeName, 1);
+            if ($previousLevel && $currentLevel > $previousLevel + 1) {
+                $issues[] = [
+                    'message' => "Header nesting - header following <h{$previousLevel}> is incorrect.",
+                    'element' =>  $heading->C14N(),
+                    'suggested_fix' => "Modify the header levels. The <h{$previousLevel}> heading should be followed by <h" . ($previousLevel + 1) . ">, not <h{$currentLevel}>.",
+                ];
+            }
+
+            $previousLevel = $currentLevel;
         }
 
         return $issues;
