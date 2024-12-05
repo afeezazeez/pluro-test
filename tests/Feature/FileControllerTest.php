6c35b53a0cc311acc9fd6a6b6675b95bec59a6f4
chillingloccini <chillingloccini1@gmail.com>
2024-12-05 01:21:50 +0100

    Created FileController Feature Test - Wrote test to that file can be uploaded and analysis(issues) and score returned

---
+++ tests/Feature/FileControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class FileControllerTest extends TestCase
+{
+
+    public function test_that_it_can_upload_html_file_and_return_analysis_results_with_issues(): void
+    {
+        $htmlContent = '<html><body><img src="image.jpg"><h1>Title</h1><div style="color: #aaa; background-color: #ccc;">Low contrast text</div></body></html>';
+
+        // Create a fake HTML file with known content
+        $htmlFile = UploadedFile::fake()->create('test_with_issues.html', 100, 'text/html');
+
+        // Write the HTML content into the fake file
+        file_put_contents($htmlFile->getRealPath(), $htmlContent);
+
+        $response = $this->json('POST', route('file.upload'), [
+            'file' => $htmlFile
+        ]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            "success" => true,
+            "message" => "Success",
+            "data" => [
+                "compliance_score" => 50,
+                "issues" => [
+                    "text_alternatives" => [
+                        [
+                            "message" => "img element missing alt attribute.",
+                            "element" => '<img src="image.jpg">',
+                            "suggested_fix" => "Add an alt attribute to your img element."
+                        ]
+                    ],
+                    "adaptable" => [],
+                    "navigable" => [],
+                    "distinguishable" => [
+                        [
+                            "message" => "The contrast between the colour of text and its background for the element is not sufficient to meet WCAG2.0 Level.",
+                            "element" => '<div style="color: #aaa; background-color: #ccc;">Low contrast text</div>',
+                            "suggested_fix" => "Use a colour contrast evaluator to determine if text and background colours provide a contrast ratio of 4.5:1 for standard text, or 3:1 for larger text. Change colour codes to produce sufficient contrast"
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+        $response->assertJsonStructure([
+            'success',
+            'message',
+            'data' => [
+                'compliance_score',
+                'issues' => [
+                    'text_alternatives',
+                    'adaptable',
+                    'navigable',
+                    'distinguishable',
+                ]
+            ]
+        ]);
+
+    }
+}
